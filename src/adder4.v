0c57ce01b0b3f029d82750987dd936b554879fdb
Kyle048 <kylesuchan@gmail.com>
2026-09-21 17:20:47 -0700

    attempt1 adder4.v

--- src/adder4.v
+++ src/adder4.v
@@ -13,7 +13,12 @@ module adder4(
 	      output [4:0] result);
    
 // add your code here -- you must use four instances of the full adder "fulladd", defined below
-   
+	wire cout0, cout1, cout2;
+	fulladd addc0(.x(a[0],.y(b[0],.cin(1'b0),.sum(result[0],.cout(cout0))
+	fulladd addc0(.x(a[1],.y(b[1],.cin(cout0),.sum(result[1],.cout(cout1))
+	fulladd addc0(.x(a[2],.y(b[2],.cin(cout1),.sum(result[2],.cout(cout2))
+	fulladd addc0(.x(a[3],.y(b[3],.cin(cout2),.sum(result[3],.cout(result[4]))
+							 
 endmodule
 
 module fulladd(
